78924e31ddf4a2af2a442b0da329201e1862e8e3
Rodolfo Pereira Lemos <45573426+DolphinPL@users.noreply.github.com>
2025-01-28 13:14:49 -0300

    Adição da função concluir uma tarefa

--- Programa.py
+++ Programa.py
@@ -9,17 +9,17 @@ Elaborar um gerenciador de tarefas com as seguintes funcionalidades:
 '''
 import os 
 
-#Função para limpar o terminal
-def limpar_terminal():
-    # Limpa a tela dependendo do sistema operacional
-    os.system('cls' if os.name == 'nt' else 'clear')
-
 #Criar variáveis 
 tarefas_pendentes = ["Lavar roupa", "Estudar Python", "Estudar inglês"]
 tarefas_concluidas =["Ir na academia"]
 menu = ["Adicionar tarefa", "Ver tarefa(s)", "Atualizar tarefas", "Completar tarefas", "Deletar tarefas que foram completadas", "Sair"]
 aux_tarefas_pendentes = tarefas_pendentes[:]
 aux_tarefas_concluidas = tarefas_concluidas[:]
+
+#Função para limpar o terminal
+def limpar_terminal():
+    # Limpa a tela dependendo do sistema operacional
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 #Função para mostrar o menu
 def menu_principal():
@@ -35,7 +35,7 @@ def entrada_numerica(prompt):
         except:
             print(f"Comando invalido. Por favor, tente novamente!")
 
-#Criar Função para adicionar tarefa
+#Função para adicionar tarefa
 def adicionar_tarefa():
     global aux_tarefas_pendentes
     nova_tarefa = ""
@@ -54,17 +54,17 @@ def adicionar_tarefa():
         print(f"A tarefa {nova_tarefa} foi adicionada com SUCESSO!")
         return aux_tarefas_pendentes
 
-#Criar Função para Ver tarefa(s) adicionada
+#Função para Ver tarefa(s) adicionada
 def ver_tarefas():
-    print("******** TAREFAS ********\n")
     tamanho_pendentes = len(tarefas_pendentes) 
+    print("******** TAREFAS ********\n")
     for indice, tarefa in enumerate(tarefas_pendentes):#Tarefas não concluídas ficam com a coluna de check vazia
         print(f"[ ] {indice+ 1} - {tarefa}")
     for indice, tarefa in enumerate(tarefas_concluidas):#Coloca o sinal de check nas tarefas que ja foram concluídas
         print(f"[\u2714] {indice + 1 + tamanho_pendentes} - {tarefa}") 
-    print("\nCOMANDOS\n[1] - RETORNAR AO MENU \n[2] - FINALIZAR PROGRAMA\n")
+    print("\nCOMANDOS\n[1] - RETORNAR AO MENU \n[2] - FINALIZAR PROGRAMA")
 
-#Criar Função para Atualizar tarefas
+#Função para Atualizar tarefas
 def att_tarefas():
     global tarefas_pendentes
     global tarefas_concluidas
@@ -78,15 +78,19 @@ def att_tarefas():
     print("Tarefas atualizadas com SUCESSO!")
     
 #Criar Função para Completar tarefas
-
-#Criar Função para Deletar tarefas que foram completadas
-def deletar_concluidas():
-    global tarefas_concluidas
-    global aux_tarefas_concluidas
-    tarefas_concluidas = []
-    aux_tarefas_concluidas = tarefas_concluidas[:]
+def concluir_tarefas():
+    limpar_terminal()
+    print("******** TAREFAS CONCLUÍDAS ********\n")
+    for indice, tarefa in enumerate(aux_tarefas_pendentes):#Tarefas não concluídas ficam com a coluna de check vazia
+        print(f"{indice+ 1} - {tarefa}")
+    print("\nCOMANDOS\n[0] - RETORNAR AO MENU")
     
-#Criar Função para Sair
+#Função para Deletar tarefas que foram completadas
+def deletar_concluidas():
+    global aux_tarefas_concluidas
+    aux_tarefas_concluidas = []
+    
+#Função para Sair
 def sair():
     print("Programa Finalizado!!!") #Apenas exibe a mensagem para informar o usuário 
     exit()
@@ -102,12 +106,12 @@ def main():
         menu_principal()
         validar = entrada_numerica("\nSelecione uma ação (Digite o número correspondente): ")
 
-    if validar == 1:
+    if validar == 1: #Para adicionar tarefa
         limpar_terminal()
         adicionar_tarefa()
         main()
         
-    elif validar == 2:
+    elif validar == 2: #Para Vizualizar as tarefas
         limpar_terminal()
         ver_tarefas()
         comando = entrada_numerica("\n ")
@@ -122,16 +126,34 @@ def main():
         else: 
             sair()
             
-    elif validar == 3:
+    elif validar == 3: #Para atualizar as tarefas
         att_tarefas()
-        main()      
+        main()    
+        
+    elif validar == 4: #Para Concluir uma tarefa
+        concluir_tarefas() 
+        comando = entrada_numerica("\nSelecione o número referente a tarefa que deseja concluir:")
+        tamanho = len(aux_tarefas_pendentes) 
+        while comando < 0 and comando > tamanho:#A função ficará repetindo até o usuário colocar um comando válido
+            limpar_terminal()
+            print("Comando inválido. Por favor, tente novamente!\n")
+            concluir_tarefas()
+            comando = entrada_numerica("\nSelecione o número referente a tarefa que deseja concluir:")
+        if comando == 0:
+            limpar_terminal()
+            main()
+        else: 
+            escolha_usuario = comando - 1
+            aux_tarefas_concluidas.append(aux_tarefas_pendentes[escolha_usuario])
+            del aux_tarefas_pendentes[escolha_usuario]
+            main()
 
-    elif validar == 5:
+    elif validar == 5: #Para deletar todas as tarefas concluídas
         deletar_concluidas()
         print("As tarefas concluídas foram excluídas.")
         main()
 
-    elif validar == 6:
+    elif validar == 6: #Para finalizar o programa
             sair()
 
 limpar_terminal()
